842472c511cb9997375e4f2062aa54176d3e18b2
mobilteknolojileri <mobilteknolojileri@gmail.com>
2026-03-22 20:12:57 +0300

    refactor(pages): remove legacy environment tags and apply strict formatting

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -12,7 +12,7 @@ export async function GET(context: any) {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: `/blog/${post.slug}`,
     })),
     customData: `<language>tr-tr</language>`,
   });
